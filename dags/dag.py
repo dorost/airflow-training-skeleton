7e279b9d764b3c0b5708cfb80b874f913b1bc201
adorostanian <adorostanian@bol.com>
2019-02-25 15:46:38 +0100

    some changes

--- dags/dag.py
+++ dags/dag.py
@@ -5,6 +5,9 @@ from airflow.operators.email_operator import EmailOperator
 from airflow.operators.python_operator import BranchPythonOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.utils.trigger_rule import TriggerRule
+from bq import BigQueryGetDataOperator
+from airflow.operators.slack_operator import SlackAPIOperator
+
 import random
 
 dag = DAG(
@@ -20,19 +23,39 @@ prints_started_job = BashOperator(
 )
 
 
-options = ['job_one', 'job_two']
+QUERY = """
+SELECT
+  name
+FROM (
+  SELECT
+    author.name AS name,
+    COUNT(*) AS count
+  FROM
+    `bigquery-public-data.github_repos.commits`
+  WHERE
+    "apache/airflow" IN UNNEST(repo_name)
+  GROUP BY
+    author.name
+  ORDER BY
+    count DESC
+  LIMIT
+    5 )
+"""
 
-def pick_a_random_branch():
-    return random.choice(options)
 
 
-branching = BranchPythonOperator(
-    task_id="prints_logs_job", python_callable=pick_a_random_branch, dag=dag
+
+get_data = BigQueryGetDataOperator(
+    task_id='get_data_from_bq',
+    sql=QUERY
 )
 
-final_task = BashOperator(
-    task_id="final_task", bash_command="echo 'done'", trigger_rule= TriggerRule.ONE_SUCCESS, dag=dag
-)
 
-for option in options: 
-    branching >> DummyOperator(task_id= option , dag=dag) >> final_task
+
+publish_to_slack = SlackAPIOperator(
+        token="xoxp-559854890739-559228586160-560304790661-ae28d681f2f1026dd05cfc0a42f27d89",
+        task_id='publish',
+        text='hello',
+        channel='General',  # Replace with your Slack username
+        username='airflow'
+)
