2df34f9a45f2df5e11b8cc4fc360548806d455bb
adorostanian <adorostanian@bol.com>
2019-02-25 14:41:05 +0100

    some changes

--- dags/dag.py
+++ dags/dag.py
@@ -2,6 +2,9 @@ import airflow
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.email_operator import EmailOperator
+from airflow.operators.python_operator import BranchPythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils.trigger_rule import TriggerRule
 
 dag = DAG(
     dag_id="hello_airflow",
@@ -16,9 +19,19 @@ prints_started_job = BashOperator(
 )
 
 
+options = ['job_one', 'job_two']
 
-prints_logs_job = BashOperator(
-    task_id="prints_logs_job", bash_command="echo 'I did the job now'", dag=dag
+def pick_a_random_branch():
+    return random.choice(options)
+
+
+branching = BranchPythonOperator(
+    task_id="prints_logs_job", python_callable=pick_a_random_branch, dag=dag
 )
 
-prints_started_job >> prints_logs_job
+final_task = BashOperator(
+    task_id="final_task", bash_command="echo 'done'", trigger_rule= TriggerRule.ONE_SUCCESS, dag=dag
+)
+
+for option in options: 
+    branching >> DummyOperator(task_id= option , dag=dag) >> final_task
