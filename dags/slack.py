dd1d0e9dd8b2d58dd672b69fb05bedb15e8aa7f0
adorostanian <adorostanian@bol.com>
2019-02-25 16:46:33 +0100

    some changes

--- dags/slack.py
+++ dags/slack.py
@@ -37,5 +37,5 @@ class MySlackAPIOperator(SlackAPIOperator):
         if not self.api_params:
             self.construct_api_call_params()
         slack = SlackHook(token=self.token, slack_conn_id=self.slack_conn_id)
-        self.api_params['text'] = data
+        self.api_params['text'] = 'Amin says: ' + str(data)
         slack.call(self.method, self.api_params)
